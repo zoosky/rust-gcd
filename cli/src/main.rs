ec966e50d347b64f90311daa5cdd2b25369140cc
Zoo Sky <andreas.kapp@swisscom.com>
2025-01-07 11:33:46 +0100

    add cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,21 @@
+use gcd_core::gcd::compute_gcd;
+struct CliArgs{
+    function: String,
+    m: u64,
+    n: u64
+}
+
 fn main() {
-    println!("Hello, world!");
+    let function = std::env::args().nth(1).expect("no pattern given");
+    let m = std::env::args().nth(2).expect("No m value given");
+    let n = std::env::args().nth(3).expect("No n value given");
+
+    let args = CliArgs{
+        function,
+        m: m.parse().unwrap(),
+        n: n.parse().unwrap()
+    };
+    let gcd = compute_gcd(args.n, args.m);
+    println!("The gcd the numbers {} and {} is {}", args.m, args.n, gcd);
+
 }
